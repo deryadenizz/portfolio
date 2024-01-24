e49605febc4b30e630d408d4572e5c908d7e3217
deryadenizz <derya.anaral@gmail.com>
2024-01-24 15:25:12 +0100

    Edited main template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+//Navbar, Header, Main, Footer
+import { PortfolioNavbarComponent } from './portfolio-navbar/portfolio-navbar.component';
+import { PortfolioHeaderComponent } from './portfolio-header/portfolio-header.component';
+import { PortfolioMainComponent } from './portfolio-main/portfolio-main.component';
+import { PortfolioFooterComponent } from './portfolio-footer/portfolio-footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PortfolioNavbarComponent,
+    PortfolioHeaderComponent,
+    PortfolioMainComponent,
+    PortfolioFooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  name = 'Derya';
+  surname = 'Deniz';
   title = 'portfolio';
 }
