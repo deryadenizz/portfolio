dcc7d03a69212a4c9ea673197e2f7140da91fa17
deryadenizz <derya.anaral@gmail.com>
2024-01-30 21:20:58 +0100

    added fontawesome icons

--- src/app/portfolio-main/portfolio-main.component.ts
+++ src/app/portfolio-main/portfolio-main.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+//Font awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPalette } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faCloudSunRain } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-portfolio-main',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './portfolio-main.component.html',
-  styleUrl: './portfolio-main.component.css'
+  styleUrl: './portfolio-main.component.css',
 })
 export class PortfolioMainComponent {
-
+  faPalette = faPalette;
+  faClock = faClock;
+  faCloudSunRain = faCloudSunRain;
 }
