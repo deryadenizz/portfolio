a2652f9d2af8286cec5acddb1334f9afe1d68c26
deryadenizz <derya.anaral@gmail.com>
2024-01-31 00:08:52 +0100

    Created footer

--- src/app/portfolio-footer/portfolio-footer.component.ts
+++ src/app/portfolio-footer/portfolio-footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-portfolio-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './portfolio-footer.component.html',
-  styleUrl: './portfolio-footer.component.css'
+  styleUrl: './portfolio-footer.component.css',
 })
 export class PortfolioFooterComponent {
-
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
